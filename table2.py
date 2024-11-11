bd8481ea3d1d7ca1e507fd47f536327fb58a9866
Hibatullah Syauqi At Taqi <108654340+hibatullahsyauqi@users.noreply.github.com>
2024-11-11 21:09:18 +0700

    readability of table2

--- table2.py
+++ table2.py
@@ -2,4 +2,4 @@ print("P     Q     P and Q    P or Q    P → Q    P ↔ Q")
 print("-" * 45)
 for P in [True, False]:
     for Q in [True, False]:
-        print(f"{P:<5} {Q:<5} {P and Q:<10} {P or Q:<10} {P <= Q:<8} {P == Q}")
+        print(f"{str(P):<5} {str(Q):<5} {str(P and Q):<10} {str(P or Q):<10} {str(P <= Q):<8} {str(P == Q)}")
